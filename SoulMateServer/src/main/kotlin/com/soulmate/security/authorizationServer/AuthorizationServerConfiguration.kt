b0e688aba6e22e79b70c80c82836eae97a60020e
Fraz <diman896@gmail.com>
2018-03-18 15:56:55 +0300

    Change client-name.

--- SoulMateServer/src/main/kotlin/com/soulmate/security/authorizationServer/AuthorizationServerConfiguration.kt
+++ SoulMateServer/src/main/kotlin/com/soulmate/security/authorizationServer/AuthorizationServerConfiguration.kt
@@ -41,13 +41,13 @@ class AuthorizationServerConfiguration : AuthorizationServerConfigurerAdapter() 
 
         clients
                 .inMemory()
-                .withClient("my-trusted-client")
+                .withClient("soulmate-client")
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
                 .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
                 .scopes("read", "write", "trust")
                 .secret("secret")
-                .accessTokenValiditySeconds(120).//Access token is only valid for 2 minutes.
-                        refreshTokenValiditySeconds(600)//Refresh token is only valid for 10 minutes.
+                .accessTokenValiditySeconds(120) //Access token is only valid for 2 minutes.
+                .refreshTokenValiditySeconds(600)//Refresh token is only valid for 10 minutes.
     }
 
     override fun configure(endpoints: AuthorizationServerEndpointsConfigurer) {
