4e670c7c79df6827ccdfbb8a1a5c621b7b562024
Fraz <diman896@gmail.com>
2018-04-22 18:51:22 +0300

    CREATED response entity for registration endpoint

--- soulmate-server/src/main/kotlin/com/soulmate/controller/RegistrationController.kt
+++ soulmate-server/src/main/kotlin/com/soulmate/controller/RegistrationController.kt
@@ -4,6 +4,8 @@ import com.soulmate.services.MemberService
 import com.soulmate.validation.registarion.RegistrationMemberValidator
 import dtos.UserRegistrationDto
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.WebDataBinder
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
@@ -24,7 +26,8 @@ class RegistrationController {
     }
 
     @PostMapping
-    fun register(@Valid @RequestBody userRegistrationDto: UserRegistrationDto) {
+    fun register(@Valid @RequestBody userRegistrationDto: UserRegistrationDto): ResponseEntity<HttpStatus> {
         memberService.registerMember(userRegistrationDto)
+        return ResponseEntity.ok().body(HttpStatus.CREATED)
     }
 }
