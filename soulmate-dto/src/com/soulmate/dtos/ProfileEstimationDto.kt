a37eeb9e4fc3adcfcdc5e105e8488e686dd262b9
Fraz <diman896@gmail.com>
2018-04-22 14:43:26 +0300

    Add default values to ProfileEstimationDto constructor

--- soulmate-dto/src/com/soulmate/dtos/ProfileEstimationDto.kt
+++ soulmate-dto/src/com/soulmate/dtos/ProfileEstimationDto.kt
@@ -1,11 +1,11 @@
 package dtos
 
-class AccountEstimationDto(
+class ProfileEstimationDto(
         id: Long = 0,
         firstName: String? = "",
         lastName: String? = "",
         profileImages: List<ProfileImageDto> = listOf(),
         gender: GenderType = GenderType.NotDefined,
         personalStory: String = "",
-        var hasAlreadyLiked: Boolean
+        var hasAlreadyLiked: Boolean = false
 ) : UserAccountDto(id, firstName, lastName, profileImages, gender, personalStory)
